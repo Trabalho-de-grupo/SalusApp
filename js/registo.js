96389175e466859bc555c0e9e76a4c4617d12e0c
Alfonso Villanueva <alfonso-40200365@esmad.ipp.pt>
2021-05-11 11:55:49 +0100

    script

--- js/registo.js
+++ js/registo.js
@@ -1,4 +1,10 @@
 function registoFunction()
-{
-    
+{ 
+    const nomeUtilizador = document.querySelector("#txtUtilizador")
+    const passUtilizador = document.querySelector("#txtPassword")
+
+    console.log(nomeUtilizador)
+    console.log(passUtilizador)
+
+    alert("oi")
 }
