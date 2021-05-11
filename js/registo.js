9eed095a91b3a89ee6939ab69c4f88c26752d080
Beatrizlopes98 <40200198@esmad.ipp.pt>
2021-05-11 17:57:26 +0100

    login

--- js/registo.js
+++ js/registo.js
@@ -1,3 +1,24 @@
+var x= document.getElementById("login");
+var y=document.getElementById("registar");
+var z= document.getElementById("btn");
+
+function registar(){
+    x.style.left ="-400px";
+    y.style.left="50px";
+    z.style.left="104px";
+}
+
+function login(){
+    x.style.left ="50px";
+    y.style.left="450px";
+    z.style.left="0";
+}
+
+
+
+
+
+
 function registoFunction()
 { 
     localStorage.setItem("nomeUtilizador", document.getElementById("txtUtilizador").value)
