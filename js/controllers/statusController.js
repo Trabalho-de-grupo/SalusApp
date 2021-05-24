60f6bd3308e414c2d5796b8a5b1df857c84796b7
Alfonso Villanueva <alfonso-40200365@esmad.ipp.pt>
2021-05-24 12:13:22 +0100

    menu

--- js/controllers/statusController.js
+++ js/controllers/statusController.js
@@ -1,3 +1,11 @@
-if (sessionStorage.getItem('loggedUser') == null ) {
+if (sessionStorage.getItem('loggedUser') == null) {
     window.location.replace('/html/registo.html')
+}
+
+function menuOn() {
+    document.getElementById("menuOverlay").style.display = "block";
+}
+
+function menuOff() {
+    document.getElementById("menuOverlay").style.display = "none";
 }
