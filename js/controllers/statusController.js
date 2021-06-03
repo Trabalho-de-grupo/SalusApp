d7e3bdbb5cb305c4400fa638bcbeb3e4b17621f6
Alfonso Villanueva <alfonso-40200365@esmad.ipp.pt>
2021-06-03 22:00:47 +0100

    verificação admin

--- js/controllers/statusController.js
+++ js/controllers/statusController.js
@@ -1,24 +1,25 @@
 if (sessionStorage.getItem('loggedUser') == null) {
     window.location.replace('/html/registo.html')
 } else {
-    
+
     users = localStorage.users ? JSON.parse(localStorage.users) : []
     utilizador = sessionStorage.getItem('loggedUser')
 
     Object.values(users).forEach(user => {
-        if(user.username == utilizador && user.status == "admin") {
+        if (user.username == utilizador && user.status == "admin") {
             btnAdmin = document.querySelectorAll(".botaoConta")
             btnAdmin[0].style.visibility = "hidden";
             btnAdmin[1].style.visibility = "visible";
         }
-
-        if (user.username == utilizador && user.status == " user" && window.location.href.split("/").pop() == "admin.html") {
-            window.location.replace('/html/conta.html')
-        }       
+        if (user.username == utilizador) {
+            status = user.status
+        }
     })
+
+    if (window.location.href.split("/").pop() == "admin.html" && status != "admin") {
+        window.location.replace('../html/perfil.html')
+    }
 }
-
-
 
 function menuOn() {
     document.getElementById("menuOverlay").style.display = "block";
