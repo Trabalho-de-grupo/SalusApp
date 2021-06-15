aea9bcc407e51b9112394b703ec9de99ce5a3a25
Alfonso Villanueva <alfonso-40200365@esmad.ipp.pt>
2021-06-15 03:09:06 +0100

    CHANGE APP.js

--- js/app.js
+++ js/app.js
@@ -4,13 +4,14 @@ import CategoriaView from './views/CategoriaView.js'
 import UtilizadorView from './views/UtilizadorView.js'
 import PageCategoriaView from './views/PageCategoriaView.js';
 import EstatisticaView from './views/EstatisticaView.js';
+import AtividadeViews from './views/AtividadesViews.js';
 
 class App {
     constructor() {
         this.routes = {
             'registo': [UserView],
             'perfil': [PerfilView],
-            'admin': [UtilizadorView, CategoriaView],
+            'admin': [UtilizadorView, CategoriaView, AtividadesViews],
             'categoria': [PageCategoriaView],
             'estatistica': [EstatisticaView]
         };
@@ -43,42 +44,63 @@ class App {
 
         const categorias = [{
                 id: 1,
-                name: 'Yoga',
+                name: 'YOGA',
                 color: 'F6B8C3',
                 icon: 'exercise.png',
                 description: `É a prática que tem como objetivo controlar o stress, ansiedade, dores no corpo, melhorar o equilíbrio e promover a sensação de bem estar.`,
             },
             {
                 id: 2,
-                name: 'Receitas',
+                name: 'RECEITAS',
                 color: 'B8B8B8',
                 icon: 'cooking.png',
                 description: `Para se inspirar, aprender a cozinhar, experimentar ideias novas e descobrir pratos saborosos, rápidos, fáceis, económicos e saudáveis.`,
             },
             {
                 id: 3,
-                name: 'Jardinagem',
+                name: 'JARDINAGEM',
                 color: 'E0B0FF',
                 icon: 'exercise.png',
                 description: `A jardinagem traz benefícios para a saúde física e mental. Estudos indicam até que é positiva para a destreza física, mental e autoestima dos mais velhos. Além disso, agrada a pequenos e graúdos e não tem contraindicações. Seja no jardim ou na varanda, experimente.`,
             },
             {
                 id: 4,
-                name: 'Artes Plásticas',
+                name: 'ARTES PLÁSTICAS',
                 color: 'C8A2C8',
                 icon: 'artist.png',
                 description: `Associar práticas artísticas à vida é uma forma de beneficiar a sua saúde, tanto física quanto mental — trata-se da chamada arteterapia, que consiste no uso de recursos artísticos/visuais ou expressivos como elemento terapêutico.`,
             }
         ];
 
+        const atividades = [{
+            id: 1,
+            name: 'Yoga',
+            color: 'F6B8C3',
+            icon: 'exercise.png',
+            description: `É a prática que tem como objetivo controlar o stress, ansiedade, dores no corpo, melhorar o equilíbrio e promover a sensação de bem estar.`,
+            },
+            {   
+            id: 2,
+            name: 'Receitas',
+            color: 'B8B8B8',
+            icon: 'cooking.png',
+            description: `Para se inspirar, aprender a cozinhar, experimentar ideias novas e descobrir pratos saborosos, rápidos, fáceis, económicos e saudáveis.`,
+            }
+        ];
+        
+        //Criaçao do Object Users no LocalStorage
+        if (!localStorage.users) {
+            localStorage.setItem('users', JSON.stringify(users));
+        }
+
         //Criaçao do Object Categorias no LocalStorage
         if (!localStorage.categorias) {
             localStorage.setItem('categorias', JSON.stringify(categorias));
         }
 
-        //Criaçao do Object Users no LocalStorage
-        if (!localStorage.users) {
-            localStorage.setItem('users', JSON.stringify(users));
+        //Criaçao do Object Atividades no LocalStorage
+        if (!localStorage.atividades) {
+            localStorage.setItem('atividades', JSON.stringify(atividades));
         }
     }
 
